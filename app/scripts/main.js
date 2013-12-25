095867a12c20ce045af02ad5c6153444676f3170
Jim Yuan <jim@home.com>
2013-12-26 01:23:13 +0800

    shu8:go on

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,25 +1,49 @@
 +function($){ "use strict";
   var MakeSwipe={
     createNew: function(){
-      var makeswipe={
-        b   :  $("section"),      //滑动页
-        w   :  $("#wrap"),        //定长容器
-        c   :  $("#wrapContent"), //滑动页包裹
-        swp :  0                  //滑动页次
-      };
+      var makeswipe={};
+
+      var wh=window.innerHeight,//滑动单位长度
+          b =  $("section"),      //滑动页
+          w =  $("#wrap"),        //定长容器
+          c =  $("#wrapContent"), //滑动页包裹
+          curTop, 
+          distY, startY;
       makeswipe.init=function(){
-        this.b.style("height", window.innerHeight+"px");
-        this.w.style("height", this.b.height()+"px");
-        this.c.style("height", this.b.height()*this.b.length+"px");
+        b.style("height", wh+"px");
+        w.style("height", wh+"px");
+        c.style("height", wh*b.length+"px");
       }
       makeswipe.doSwipe=function(){
-        var c=this.c;
+        var pageNum=0;
         this.init();
-        c.on("swiping", function(e){
-          c.style("margin-top", e.currentTouch.y+"px");
+        c.on("touchstart", function(e){
+          curTop=parseInt(c.style("margin-top"), 10);
+          startY=e.pageY;
+          // console.log(startY)
+          $(this).removeClass('T');
+
+        }).on("touchmove",function(e){
+          distY=startY-e.pageY;
+          console.log(distY)
+          c.style("margin-top", curTop+distY*0.6+"px");
+
+        }).on("touchend",function(){
+          c.style("margin-top", -wh*pageNum+"px");
+          $(this).addClass('T')
+
         }).on("swipeUp", function(e){
-          console.log(e);
-        });
+          if(pageNum<b.length-1){
+            pageNum++;
+            c.style("margin-top", -wh*pageNum+"px")
+          }
+
+        }).on("swipeDown", function(){
+          if(pageNum>0){
+            pageNum--;
+            c.style("margin-top", -wh*pageNum+"px");
+          }
+        })
       }
 
       return makeswipe;
